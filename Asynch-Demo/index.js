93b65f8b0f77d35ce16f9d86212f65cc58910ed5
JahedHossenBangladesh <jahedhossen4358.com>
2021-04-20 21:32:14 +0600

    This is modify of promise

--- Asynch-Demo/index.js
+++ Asynch-Demo/index.js
@@ -1,21 +1,42 @@
 console.log('before');
-getUser(1,(user) =>{
-    console.log('User',user.id)
-    getRepo(user.githubUser,function(repo){
-        console.log('repo',repo)
-    })
-});
-console.log('after');
+// getUser(1,(user) =>{
+//     console.log('User',user.id)
+//     getRepo(user.githubUser,function(repo){
+//         console.log('repo',repo)
+//     })
+// });
 
-function getUser(id,caallback){
+ getUser(1)
+.then(user => getRepo(user.githubUser))
+.then(repo => getCommits(repo[0]))
+.then(commit => console.log('commit',commit));
+
+
+console.log("after");
+
+function getUser(id){
+    return new Promise((resolve,reject) =>{
 setTimeout(() => {
   console.log(`it is asynco ${id}`);
-  caallback({ id: id,githubUser: "jahed"});
+  resolve({ id: id, githubUser: "jahed" });
 }, 2000);
-return 1;
+    })
+
+
 }
 
-function getRepo(users,called){
-    setTimeout(() =>{ called(["a", "b", "c"]);
- },2000);
+function getRepo(users){
+  return new Promise((resolve,reject) =>{
+  setTimeout(() => {
+    resolve(["a", "b", "c"]);
+  }, 2000);
+  })
+}
+
+function getCommits(repo){
+    return new Promise((resolve,reject) =>{
+        setTimeout(() => {
+            resolve(['commit'])
+        })
+    })
 }
